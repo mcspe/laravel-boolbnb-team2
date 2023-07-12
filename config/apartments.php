57b40f5519a5bec177a16cda83cf19e6024daaff
raffaele-catalano <raf.catalano@gmail.com>
2023-07-12 18:08:53 +0200

    created apartments data arrays

---
+++ config/apartments.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    [
+        "title" => "Casa Rustica con Piscina",
+        "slug" => "casa-rustica-con-piscina",
+        "category" => "Casa",
+        "address" => "Via del Carmine, 18, Pozzuoli",
+        "n_rooms" => 2,
+        "n_bathrooms" => 2,
+        "n_beds" => 2,
+        "square_meters" => 80,
+        "latitude_longitude" => "43.55222, -87.9617585",
+        "price" => 400.00,
+        "cover_image" => "https://picture.com/house-pool",
+        "is_visible" => "1"
+    ],
+    [
+        "title" => "Appartamento con Vista",
+        "slug" => "appartamento-con-vista",
+        "category" => "Appartamento",
+        "address" => "Via del Poggio, 48, Bacoli",
+        "n_rooms" => 3,
+        "n_bathrooms" => 2,
+        "n_beds" => 2,
+        "square_meters" => 100,
+        "latitude_longitude" => "44.52592, -96.9665785",
+        "price" => 650.00,
+        "cover_image" => "https://picture.com/apartments-view",
+        "is_visible" => "0"
+    ],
+    [
+        "title" => "Bed and Breakfast Luxury",
+        "slug" => "bed-and-breakfast-luxury",
+        "category" => "BnB",
+        "address" => "Via Orazio, 18, Napoli",
+        "n_rooms" => 3,
+        "n_bathrooms" => 2,
+        "n_beds" => 2,
+        "square_meters" => 70,
+        "latitude_longitude" => "89.53622, -65.2587585",
+        "price" => 800.00,
+        "cover_image" => "https://picture.com/bnb-luxury",
+        "is_visible" => "1"
+    ],
+];
