b5bedffba24e3d4299985d3262e0de44361fb94e
marcocnc <marcocancelliere56@gmail.com>
2023-07-12 17:37:57 +0200

    populated seeder and finished services table

--- config/room-services.php
+++ config/room-services.php
@@ -1,21 +1,21 @@
 <?php
 
 return [
-    [
-        'Cucina',
-        'Piscina',
-        'Parcheggio',
-        'TV',
-        'Aria condizionata',
-        'Lavatrice',
-        'Colazione',
-        'Ferro da stiro',
-        'Asciugatrice',
-        'Riscaldamento',
-        'Camino',
-        'Idromassaggio',
-        'Culla',
-        'Griglia per barbecue',
-        'Palestra',
-    ]
+
+    'Cucina',
+    'Piscina',
+    'Parcheggio',
+    'TV',
+    'Aria condizionata',
+    'Lavatrice',
+    'Colazione',
+    'Ferro da stiro',
+    'Asciugatrice',
+    'Riscaldamento',
+    'Camino',
+    'Idromassaggio',
+    'Culla',
+    'Griglia per barbecue',
+    'Palestra'
+
 ];
--- database/seeders/ServicesTableSeeder.php
+++ database/seeders/ServicesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Service;
 
 class ServicesTableSeeder extends Seeder
 {
@@ -14,6 +15,11 @@ class ServicesTableSeeder extends Seeder
      */
     public function run()
     {
-
+        $services = config('room-services');
+        foreach ($services as $service){
+            $newService = new Service();
+            $newService->name = $service;
+            $newService->save();
+        }
     }
 }
