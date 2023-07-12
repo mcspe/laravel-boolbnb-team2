8405cb1223f5430413f287a65177bcb3930f32c4
mirkettinho <mirkodifranco30@gmail.com>
2023-07-12 17:38:49 +0200

    created messages model & seeder

---
+++ database/seeders/MessagesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Message;
+
+
+class MessagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $messages = config("messages");
+
+        foreach($messages as $message){
+            $new_message = new Message();
+            $new_message->sender_name = $message["sender_name"];
+            $new_message->sender_lastname = $message["sender_lastname"];
+            $new_message->sender_email = $message["sender_email"];
+            $new_message->text = $message["text"];
+            $new_message->save();
+
+        }
+    }
+}
