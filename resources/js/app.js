7bf63b692ca4301d99df72a43e66407312c70262
mirkettinho <mirkodifranco30@gmail.com>
2023-07-13 16:20:46 +0200

    create home admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,10 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/v4-shims.scss';
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.admin')
+
+@section('content')
+
+
+
+<div class="bg-home">
+  <div class="welcome">
+
+    <h1>ciao {{ Auth::user()->name }}! <i class="fa-solid fa-hand fa-shake" style="color: #ffd500;"></i></h1>
+    <h4>Qui sono presenti tutte le statistiche della tua attività</h4>
+  </div>
+
+  <div class="d-flex justify-content-center">
+
+    <div class="box-card d-flex">
+      <div class="circle-img text-center green-circle">
+        <i class="fa-solid fa-house-user"></i>
+      </div>
+        <div class="card-md-description">
+          <span>Appartamenti</span>
+          <div class="bold">
+            <span>110</span>
+          </div>
+        </div>
+    </div>
+
+    <div class="box-card d-flex ">
+      <div class="circle-img text-center blue-circle ">
+        <i class="fa-solid fa-list-ol"></i>
+      </div>
+        <div class="card-md-description ">
+          <span>Elenco Appartamenti</span>
+          <div class="bold">
+            <span>1</span>
+          </div>
+        </div>
+    </div>
+
+
+    <div class="box-card d-flex">
+      <div class="circle-img text-center red-circle">
+        <i class="fa-solid fa-signal"></i>
+      </div>
+        <div class="card-md-description">
+          <span>Visite</span>
+          <div class="bold">
+            <span>333</span>
+          </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
+
+
+
+
+
+
