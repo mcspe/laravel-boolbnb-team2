8676813ca5fcf5b9b8a3564d592922ad2181e7cf
mcspe <mcspezzaferro@gmail.com>
2023-07-13 10:30:37 +0200

    moved apt seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ApartmentsTableSeeder::class,
             UsersTableSeeder::class,
             ServicesTableSeeder::class,
             MessagesTableSeeder::class,
             SponsorshipsTableSeeder::class,
+            ApartmentsTableSeeder::class,
         ]);
     }
 }
