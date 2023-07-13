0be4033055004c4e1068a85aa3f217d7e3c4e135
mcspe <mcspezzaferro@gmail.com>
2023-07-13 17:08:08 +0200

    [add] created relationships

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    public function apartment() {
+        return $this->belongsTo(Apartment::class);
+    }
 }
---
+++ database/migrations/2023_07_13_143027_create_apartment_service_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_service', function (Blueprint $table) {
+            $table->unsignedBigInteger('apartment_id')->nullable();
+	        $table->foreign('apartment_id')->references('id')->on('apartments')->onDelete('set null');
+
+            $table->unsignedBigInteger('service_id')->nullable();
+	        $table->foreign('service_id')->references('id')->on('services')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_service');
+    }
+};
