7526fa428a0c928f00dc72025093072e0bc2a0ee
mcspe <mcspezzaferro@gmail.com>
2023-07-14 15:26:42 +0200

    [fix] created pivot model to fix error

--- app/Models/ApartmentSponsorship.php
+++ app/Models/ApartmentSponsorship.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class ApartmentSponsorship extends Model
+class ApartmentSponsorship extends Pivot
 {
-    use HasFactory;
+    //
 }
