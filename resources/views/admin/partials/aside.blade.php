eb62d462b08092d8ba535549c0e3cf7617e0208c
mirkettinho <mirkodifranco30@gmail.com>
2023-07-14 23:09:52 +0200

    change aside logo

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -5,13 +5,18 @@
           <li><a href="{{ route('admin.home') }}" class="{{Route::currentRouteName()=== "admin.home" ? "active" : ""}}"><i class="fa-solid fa-chart-line"></i> Dashboard</a></li>
           <li><a href="{{route("admin.apartments.create")}}" class="{{Route::currentRouteName()=== "admin.apartments.create" ? "active" : ""}}"><i class="fa-solid fa-square-plus"></i> Aggiungi Appartamento</a></li>
           <li><a href="{{route("admin.apartments.index")}}" class="{{Route::currentRouteName()=== "admin.apartments.index" ? "active" : ""}}"><i class="fa-solid fa-list"></i> Elenco Appartamenti</a></li>
+          <li><a href="#"><i class="fa-solid fa-globe"></i>Visita il sito pubblico</a></li>
+
+          <div class="line"></div>
+
+
           <li class="nav-item dropdown d-flex">
 
-            <span class="text-white me-2">{{ Auth::user()->name }}</span>
+            <span class=""><i class="fa-regular fa-user"></i>{{ Auth::user()->name }}</span>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" >
                 @csrf
-                <button class="btn btn-secondary" type="submit" title="logout"><i class="fa-solid fa-arrow-right-from-bracket"></i></button>
+                <button class="btn btn-danger" type="submit" title="logout"><i class="fa-solid fa-arrow-right-from-bracket"></i></button>
             </form>
 
         </li>
