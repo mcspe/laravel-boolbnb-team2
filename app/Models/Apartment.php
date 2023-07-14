a7b3378e3f3a7efb5ccf400aa2fb7eba1a1f22ad
marcocnc <marcocancelliere56@gmail.com>
2023-07-14 12:40:37 +0200

    implemented function to get coordinates

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Apartment extends Model
 {
@@ -27,5 +28,40 @@ class Apartment extends Model
 
     public function messages() {
         return $this->hasMany(Message::class);
+    }
+
+    public static function getCoordinates($address) {
+
+      /*
+      First attempt
+      $exampleQuerySearch = 'https://api.tomtom.com/search/2/search/via%20carlo%20verre%2044.json?typeahead=false&limit=1&view=Unified&key=jMP7C6DHaaq8PNVgJUg740ueeMPlH0xY';
+      */
+
+      // URL Base
+      $baseUrl = 'https://api.tomtom.com/search/2/search/';
+
+      // Function for slug the address
+      $addressToSearch = Str::slug($address, '%20');
+
+      // Type of query which returns a json
+      $query = '.json?typeahead=false&limit=1&view=Unified&key=';
+
+      // Api key
+      $apiKey = 'jMP7C6DHaaq8PNVgJUg740ueeMPlH0xY';
+
+      // Receive contents in a json
+      $addressJsonResults = file_get_contents($baseUrl . $addressToSearch . $query . $apiKey);
+
+      // Decoding json
+      $decodingAddress = json_decode($addressJsonResults, JSON_PRETTY_PRINT);
+
+      // Get latitude and longitude from json array
+      $latitude = $decodingAddress['results'][0]['position']['lat'];
+      $longitude = $decodingAddress['results'][0]['position']['lon'];
+
+      // Get coordinates with DB syntax
+      $coordinates = "ST_GeomFromText('POINT($latitude $longitude)')";
+
+      return $coordinates;
     }
 }
