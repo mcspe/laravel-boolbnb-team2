3b2eca4c9ae22dd653f77f9daac24497b76ed366
mirkettinho <mirkodifranco30@gmail.com>
2023-07-14 23:10:27 +0200

    change cover_image path

--- config/apartments.php
+++ config/apartments.php
@@ -12,7 +12,7 @@ return [
         "square_meters" => 80,
         // "latitude_longitude" => "ST_GeomFromText('POINT(43.55222 -87.9617585)')",
         "price" => 400.00,
-        "cover_image" => "https://picture.com/house-pool",
+        "cover_image" => "https://a0.muscache.com/im/pictures/miso/Hosting-673594157711193389/original/c2708998-b4b9-478f-9baf-b90244944f93.jpeg?im_w=1200",
         "is_visible" => "1"
     ],
     [
@@ -26,21 +26,21 @@ return [
         "square_meters" => 100,
         // "latitude_longitude" => "ST_GeomFromText('POINT(44.52592 -96.9665785)')",
         "price" => 650.00,
-        "cover_image" => "https://picture.com/apartments-view",
+        "cover_image" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-45967096/original/5eeb3266-1f26-44c1-84f1-82a2968cfdd5.jpeg?im_w=1200",
         "is_visible" => "0"
     ],
     [
-        "title" => "Bed and Breakfast Luxury",
-        // "slug" => "bed-and-breakfast-luxury",
+        "title" => "Bed and Breakfast",
+        // "slug" => "bed-and-breakfast",
         "category" => "BnB",
-        "address" => "Via Orazio, 18, Napoli",
+        "address" => "Vico Cariati, 55, Napoli",
         "n_rooms" => 3,
         "n_bathrooms" => 2,
         "n_beds" => 2,
         "square_meters" => 70,
         // "latitude_longitude" => "ST_GeomFromText('POINT(89.53622 -65.2587585)')",
         "price" => 800.00,
-        "cover_image" => "https://picture.com/bnb-luxury",
+        "cover_image" => "https://cf.bstatic.com/xdata/images/hotel/max1024x768/436213512.jpg?k=1af5c5f1aa555e6023b1725a9176449488744c31a27620f5512f28f62a09483e&o=&hp=1",
         "is_visible" => "1"
     ],
 ];
