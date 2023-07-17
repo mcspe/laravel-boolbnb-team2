a5e81d05d579d74984f340e85cff0391134618ce
marcocnc <marcocancelliere56@gmail.com>
2023-07-17 10:38:10 +0200

    configured index function

--- app/Http/Controllers/SponsorshipsController.php
+++ app/Http/Controllers/SponsorshipsController.php
@@ -14,7 +14,8 @@ class SponsorshipsController extends Controller
      */
     public function index()
     {
-        //
+        $sponsorships = Sponsorship::all();
+        return view('admin.sponsorships.index', compact('sponsorships'));
     }
 
     /**
