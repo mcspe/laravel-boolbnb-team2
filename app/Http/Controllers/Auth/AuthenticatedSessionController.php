ea7897d60933a4560611e5e2e5c6e65d72127e2d
raffaele-catalano <raf.catalano@gmail.com>
2023-07-26 20:05:59 +0200

    added a new redirect at logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -43,6 +43,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/login-register');
     }
 }
