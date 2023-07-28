1f984289ab607394c1d25519467694c4431cd644
mcspe <mcspezzaferro@gmail.com>
2023-07-29 01:30:35 +0200

    [add] valid or invelid card processing

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 use Braintree\Transaction;
 use Braintree\Gateway;
 use App\Models\Sponsorship;
@@ -46,39 +47,70 @@ class PaymentController extends Controller
         'privateKey' => config('services.braintree.private_key'),
       ]);
 
-      // Esegui il pagamento tramite Braintree
-      $result = $gateway->transaction()->sale([
-        'amount' => $sponsorship->price, // Importo della sponsorizzazione
-        'paymentMethodNonce' => $nonce,
-        'options' => [
-          'submitForSettlement' => true
-        ]
-      ]);
-
-      // Gestisci il risultato della transazione e invia una risposta al frontend
-      if ($result->success) {
-        $sponsorship->apartments()->save($apartment, [
-          'payment_date' => $payment_date,
-          'expiration_date' => $expiration_date,
-          'created_at' => Carbon::now(),
-          'updated_at' => Carbon::now()
+      // Creo un cliente per autorizzare la verifica
+        $newCustomer = $gateway->customer()->create([
+          'id' => 'BnB_'.Auth::id(),
+          'firstName' => Auth::user()->name,
+          'lastName' => Auth::user()->lastname,
+          'email' => Auth::user()->email
         ]);
-        // return redirect()->route('admin.sponsorship', $apartment)->with('sponsorship_flag', true)->with('message', 'Complimenti! Il tuo acquisto è andato a buon fine!');
-        // return response()->json(['success' => true, 'message' => 'Complimenti! Il tuo acquisto è andato a buon fine!', 'redirect_url' => "route('admin.sponsorship', $apartmentId)"]);
-        $response = response()->json(['success' => true, 'message' => 'Complimenti! Il tuo acquisto è andato a buon fine!']);
-        $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+
+      // Creo una transazione di vendita
+      // $saleTransaction = $gateway->transaction()->sale([
+      //   'amount' => $sponsorship->price, // Importo della sponsorizzazione
+      //   'paymentMethodNonce' => $nonce,
+      //   // 'customer' => [
+      //   //   'id' => 'BnB_'.Auth::id(),
+      //   //   'firstName' => Auth::user()->name,
+      //   //   'lastName' => Auth::user()->lastname,
+      //   //   'email' => Auth::user()->email,
+      //   // ],
+      //   'options' => [
+      //     'submitForSettlement' => true,
+      //     'storeInVaultOnSuccess' => true
+      //   ]
+      // ]);
+
+      // Gestisco il risultato della transazione
+      // if ($saleTransaction->success) {
+
+        // Verifica del metodo di pagamento
+        $cardVerification = $gateway->paymentMethod()->create([
+          'customerId' => 'BnB_' . Auth::id(),
+          'paymentMethodNonce' => $nonce,
+          'options' => [
+            'verifyCard' => true
+          ]
+        ]);
+
+        $success = $cardVerification->success;
+
+        // Controllo il succeso della verifica
+        if ($success) {
+          $message = $cardVerification;
+        } else {
+          $message = $cardVerification;
+        }
+
+        $response = response()->json($message);
+
+        // $response = response()->json(['success' => true, 'message' => 'Complimenti! Il tuo acquisto è andato a buon fine!', 'info'=> $transactionMessage]);
+        // $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
         return $response;
-      } else {
+      // } else {
+      //   return response()->json($saleTransaction->message);
+
         // visualizzo in console il messaggio
-        Log::error($result->message);
-        return response()->json(['success' => false, 'message' => 'Purtroppo sembra ci sia qualche problema con il pagamento, controlla che tutti i campi inseriti siano corretti.']);
-      }
+        // Log::error($transactionResult->message);
+        // return response()->json(['success' => false, 'message' => 'Purtroppo sembra ci sia qualche problema con il pagamento, controlla che tutti i campi inseriti siano corretti.']);
+      // }
     } catch (\Exception $e) {
       // Cattura l'eccezione e loggala nel file di log
       Log::error($e->getMessage());
 
+      $errorMessage = $e->getMessage();
       // Ottieni il messaggio di errore dall'eccezione
-      $errorMessage = "Qualcosa è andato storto con il tuo pagamento. Siamo al lavoro per risolvere il problema. Ci scusiamo per il disagio.";
+      // $errorMessage = "Qualcosa è andato storto con il tuo pagamento. Siamo al lavoro per risolvere il problema. Ci scusiamo per il disagio.";
 
       // Invia una risposta di errore al frontend
       return response()->json(['success' => false, 'message' => $errorMessage]);
