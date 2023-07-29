8630e8879629b77e8989acd1c5d7a063b3f127a9
mcspe <mcspezzaferro@gmail.com>
2023-07-29 14:52:06 +0200

    [add] payment logic working

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -17,27 +17,12 @@ class PaymentController extends Controller
 {
   public function checkout(Request $request){
     try {
-      $nonce = $request->input('paymentMethodNonce');
 
       $apartmentId = $request->input('apartmentId');
       $apartment = Apartment::where('id', $apartmentId)->first();
 
       $sponsorshipId = $request->input('sponsorshipId');
       $sponsorship = Sponsorship::where('id', $sponsorshipId)->first();
-
-      $expiration_date = Carbon::now();
-      switch ($sponsorshipId) {
-        case '1':
-          $expiration_date->addHours(24);
-          break;
-        case '2':
-          $expiration_date->addHours(72);
-          break;
-        case '3':
-          $expiration_date->addHours(144);
-          break;
-      }
-      $payment_date = Carbon::now();
 
       // Crea un'istanza di Gateway con i parametri di configurazione
       $gateway = new Gateway([
@@ -46,71 +31,102 @@ class PaymentController extends Controller
         'publicKey' => config('services.braintree.public_key'),
         'privateKey' => config('services.braintree.private_key'),
       ]);
+      $nonce = $request->input('paymentMethodNonce');
 
       // Creo un cliente per autorizzare la verifica
-        $newCustomer = $gateway->customer()->create([
-          'id' => 'BnB_'.Auth::id(),
-          'firstName' => Auth::user()->name,
-          'lastName' => Auth::user()->lastname,
-          'email' => Auth::user()->email
-        ]);
+      $customer = $gateway->customer()->create();
+      $customerId = $customer->customer->id;
+      // do {
+      //   $customer = $gateway->customer()->find('testId');
 
-      // Creo una transazione di vendita
-      // $saleTransaction = $gateway->transaction()->sale([
-      //   'amount' => $sponsorship->price, // Importo della sponsorizzazione
-      //   'paymentMethodNonce' => $nonce,
-      //   // 'customer' => [
-      //   //   'id' => 'BnB_'.Auth::id(),
-      //   //   'firstName' => Auth::user()->name,
-      //   //   'lastName' => Auth::user()->lastname,
-      //   //   'email' => Auth::user()->email,
-      //   // ],
-      //   'options' => [
-      //     'submitForSettlement' => true,
-      //     'storeInVaultOnSuccess' => true
-      //   ]
-      // ]);
+      //   if (!$customer->success) {
+      //     $newCustomer = $gateway->customer()->create([
+      //       'id' => 'testId',
+      //       // 'firstName' => Auth::user()->name,
+      //       // 'lastName' => Auth::user()->lastname,
+      //       // 'email' => Auth::user()->email
+      //     ]);
 
-      // Gestisco il risultato della transazione
-      // if ($saleTransaction->success) {
+      //     if (!$newCustomer->success) {
+      //       $message = 'Problemi creazione cliente - Qualcosa è andato storto con il tuo pagamento. Siamo al lavoro per risolvere il problema. Ci scusiamo per il disagio.';
+      //       $customer = 1; // forza l'uscita dal ciclo in caso di errore
+      //     }
+      //   }
+      // } while ($customer === null);
 
-        // Verifica del metodo di pagamento
-        $cardVerification = $gateway->paymentMethod()->create([
-          'customerId' => 'BnB_' . Auth::id(),
-          'paymentMethodNonce' => $nonce,
+      // Verifica del metodo di pagamento
+      $cardVerification = $gateway->paymentMethod()->create([
+        'customerId' => $customerId,
+        'paymentMethodNonce' => $nonce,
+        'options' => [
+          'verifyCard' => true
+        ]
+      ]);
+
+      $verificationSuccess = $cardVerification->success;
+
+      // Controllo il succeso della verifica
+      if ($verificationSuccess) {
+        $paymentMethodToken = $cardVerification->paymentMethod->token;
+
+        //Creo una transazione di vendita
+        $saleTransaction = $gateway->transaction()->sale([
+          'amount' =>  $sponsorship->price, // Importo della sponsorizzazione
+          'paymentMethodToken' => $paymentMethodToken,
           'options' => [
-            'verifyCard' => true
+            'submitForSettlement' => true
           ]
         ]);
 
-        $success = $cardVerification->success;
+        // Gestisco il risultato della transazione
+        if ($saleTransaction->success) {
 
-        // Controllo il succeso della verifica
-        if ($success) {
-          $message = $cardVerification;
+          $expiration_date = Carbon::now();
+          switch ($sponsorshipId) {
+            case '1':
+              $expiration_date->addHours(24);
+              break;
+            case '2':
+              $expiration_date->addHours(72);
+              break;
+            case '3':
+              $expiration_date->addHours(144);
+              break;
+          }
+          $payment_date = Carbon::now();
+
+          /// aggiunta sponsorizzazione
+          $sponsorship->apartments()->save($apartment, [
+            'payment_date' => $payment_date,
+            'expiration_date' => $expiration_date,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+          ]);
+
+          $success = true;
+          $message = 'Complimenti! Il tuo acquisto è andato a buon fine!';
+
         } else {
-          $message = $cardVerification;
+          $success = false;
+          $message = "Purtroppo la transazione è stata negata. Ti consigliamo di contattare il tuo gestore per verificare il metodo di pagamento inserito";
         }
+      } else {
+        $success = false;
+        $message = "Purtroppo il metodo di pagamento da te inserito non può essere verificato. Ti consigliamo di contattare il tuo gestore per verificare quale potrebbe essere il problema";
+      }
 
-        $response = response()->json($message);
+      $response = response()->json(['success' => $success, 'message' => $message]);
+      $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
 
-        // $response = response()->json(['success' => true, 'message' => 'Complimenti! Il tuo acquisto è andato a buon fine!', 'info'=> $transactionMessage]);
-        // $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
-        return $response;
-      // } else {
-      //   return response()->json($saleTransaction->message);
+      return $response;
 
-        // visualizzo in console il messaggio
-        // Log::error($transactionResult->message);
-        // return response()->json(['success' => false, 'message' => 'Purtroppo sembra ci sia qualche problema con il pagamento, controlla che tutti i campi inseriti siano corretti.']);
-      // }
     } catch (\Exception $e) {
       // Cattura l'eccezione e loggala nel file di log
       Log::error($e->getMessage());
 
-      $errorMessage = $e->getMessage();
       // Ottieni il messaggio di errore dall'eccezione
-      // $errorMessage = "Qualcosa è andato storto con il tuo pagamento. Siamo al lavoro per risolvere il problema. Ci scusiamo per il disagio.";
+      // $errorMessage = $e->getMessage();
+      $errorMessage = "Qualcosa è andato storto con il tuo pagamento. Siamo al lavoro per risolvere il problema. Ci scusiamo per il disagio.";
 
       // Invia una risposta di errore al frontend
       return response()->json(['success' => false, 'message' => $errorMessage]);
