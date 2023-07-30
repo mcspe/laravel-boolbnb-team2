2b9483b22261486d310088fd4c5ae59de26ba118
mcspe <mcspezzaferro@gmail.com>
2023-07-30 18:22:16 +0200

    [add] minor fix in controller

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -11,30 +11,24 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Carbon;
 
-
-
-class SearchController extends Controller
-{
+class SearchController extends Controller{
     //////////////////////////////////////////// Controllare se viene utilizzata se no commentare
-  private function calculateDistance($longitude1, $latitude1, $longitude2, $latitude2) {
+  // private function calculateDistance($longitude1, $latitude1, $longitude2, $latitude2) {
 
-    $rawDistance = DB::selectOne("SELECT ST_Distance_Sphere(point($longitude1, $latitude1), point($longitude2, $latitude2)) as distance");
+  //   $rawDistance = DB::selectOne("SELECT ST_Distance_Sphere(point($longitude1, $latitude1), point($longitude2, $latitude2)) as distance");
 
-    return $rawDistance->distance / 1000;
-  }
-  ///////////////////////////////////////////
+  //   return $rawDistance->distance / 1000;
+  // }
+  // ///////////////////////////////////////////
   public function advancedSearch(Request $request) {
-  try {
-
+    try {
 
       $data = $request->all();
-
       $input = $data['input'];
       $radius = $data['radius']; // in Km
       $services = $data['services'];
       $today = Carbon::now()->format('Y-m-d H:i:s');
       $availableServices = Service::all();
-
 
       if(!$input){
         $apartments = Apartment::select([
@@ -76,7 +70,6 @@ class SearchController extends Controller
         // Receive contents in a json
         $addressJsonResults = file_get_contents($baseUrl . $addressToSearch . $query . $apiKey);
 
-
         // Decoding json
         $decodingAddress = json_decode($addressJsonResults, JSON_PRETTY_PRINT);
 
@@ -84,42 +77,40 @@ class SearchController extends Controller
         //   $apartments = [];
         //   $count = 0;
         //   response()->json()->compact('apartments', 'count', 'availableServices');
-        // } else {
-
-          // Get latitude and longitude from json array
-          $latitude = $decodingAddress['results'][0]['position']['lat'];
-          $longitude = $decodingAddress['results'][0]['position']['lon'];
-          // $longitude = $data['longitude'];
-          // $latitude = $data['latitude'];
-
-          $apartments = Apartment::select([
-              'id', 'user_id', 'title', 'slug', 'category', 'address', 'n_rooms', 'n_beds', 'n_bathrooms','square_meters', 'price', 'cover_image', 'is_visible',
-              DB::raw("ST_Y(latitude_longitude) as latitude"),
-              DB::raw("ST_X(latitude_longitude) as longitude"),
-              DB::raw("ST_Distance_Sphere(point(ST_X(latitude_longitude), ST_Y(latitude_longitude)), point($longitude, $latitude)) / 1000 as distance"),
-              DB::raw("CASE WHEN apartment_sponsorship.apartment_id IS NOT NULL THEN 1 ELSE 0 END as sponsored"), 'apartment_sponsorship.expiration_date'
-            ])
-            ->where('is_visible', '=', 1)
-            ->leftJoin('apartment_sponsorship', function ($join) use ($today) {
-              $join->on('apartments.id', '=', 'apartment_sponsorship.apartment_id')
-                ->where('apartment_sponsorship.expiration_date', '>=', $today);
-            })
-            ->with('services')
-            ->having('distance', '<=', $radius)
-            ->when(count($services) > 0, function ($query) use ($services) {
-              return $query->whereHas('services', function (Builder $query) use ($services) {
-                  $query->whereIn('service_id', $services);
-              }, '=', count($services));
-            })
-            ->orderBy('distance')
-            ->orderBy('apartment_sponsorship.expiration_date', 'desc')
-            ->get();
-
-            $count = count($apartments);
-
-            return response()->json(compact('apartments', 'count'));
-
         // }
+
+        // Get latitude and longitude from json array
+        $latitude = $decodingAddress['results'][0]['position']['lat'];
+        $longitude = $decodingAddress['results'][0]['position']['lon'];
+        // $longitude = $data['longitude'];
+        // $latitude = $data['latitude'];
+
+        $apartments = Apartment::select([
+            'id', 'user_id', 'title', 'slug', 'category', 'address', 'n_rooms', 'n_beds', 'n_bathrooms','square_meters', 'price', 'cover_image', 'is_visible',
+            DB::raw("ST_Y(latitude_longitude) as latitude"),
+            DB::raw("ST_X(latitude_longitude) as longitude"),
+            DB::raw("ST_Distance_Sphere(point(ST_X(latitude_longitude), ST_Y(latitude_longitude)), point($longitude, $latitude)) / 1000 as distance"),
+            DB::raw("CASE WHEN apartment_sponsorship.apartment_id IS NOT NULL THEN 1 ELSE 0 END as sponsored"), 'apartment_sponsorship.expiration_date'
+          ])
+          ->where('is_visible', '=', 1)
+          ->leftJoin('apartment_sponsorship', function ($join) use ($today) {
+            $join->on('apartments.id', '=', 'apartment_sponsorship.apartment_id')
+              ->where('apartment_sponsorship.expiration_date', '>=', $today);
+          })
+          ->with('services')
+          ->having('distance', '<=', $radius)
+          ->when(count($services) > 0, function ($query) use ($services) {
+            return $query->whereHas('services', function (Builder $query) use ($services) {
+                $query->whereIn('service_id', $services);
+            }, '=', count($services));
+          })
+          ->orderBy('distance')
+          ->orderBy('apartment_sponsorship.expiration_date', 'desc')
+          ->get();
+
+        $count = count($apartments);
+
+        return response()->json(compact('apartments', 'count', 'availableServices'));
 
       }
 
@@ -133,27 +124,5 @@ class SearchController extends Controller
       }
       return response()->json(['error' => $th->getMessage()], 500);
     }
-    }
-    }
-
-
-      // if (count($services) > 0) {
-      //   $query->whereHas('services', function (Builder $query) use ($services) {
-      //       $query->whereIn('service_id', $services);
-      //   });
-      // }
-
-
-
-
-      // $filteredApartments = $apartments->filter(function ($apartment) use ($longitude, $latitude, $radius) {
-      //     $distanceInKm = $this->calculateDistance($apartment->longitude, $apartment->latitude, $longitude, $latitude);
-      //     return $distanceInKm <= $radius;
-      // })->values()->toArray();
-
-
-
-
-
-      // return response()->json(compact('filteredApartments'));
-
+  }
+}
